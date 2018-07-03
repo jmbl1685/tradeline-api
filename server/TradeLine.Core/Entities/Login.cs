835854670f4f59b54c17e011eb29c5d8cf450e0c
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-07-03 10:18:45 -0500

    JB - Add TradeLine.Entities

---
+++ server/TradeLine.Core/Entities/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeLine.Core.Entities
+{
+    public class Login
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ server/TradeLine.Core/Entities/User.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TradeLine.Entities
+{
+    public class User
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public User()
+        {
+
+        }
+
+        [JsonProperty("_id")]
+        [Required]
+        public string UserCode { get; set; }
+
+        [JsonProperty("name")]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonProperty("lastname")]
+        [Required]
+        public string Lastname { get; set; }
+
+        [JsonProperty("username")]
+        [Required]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        [Required]
+        public string Password { get; set; }
+
+        [JsonProperty("email")]
+        [DataType(DataType.EmailAddress), EmailAddress, Required]
+        public string Email { get; set; }
+
+        [JsonProperty("imageurl")]
+        [Required]
+        public string ImageURL { get; set; }
+
+    }
+}
